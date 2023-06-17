c982a4750ef92d3583d3e847ee86d41ebf9e4314
HugoCharles997 <hugo.charles.pro997@gmail.com>
2023-06-17 23:25:23 +0200

    add twriter effect

--- src/app/views/homepage/homepage.component.ts
+++ src/app/views/homepage/homepage.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent {
+  constructor() {}
 
+  ngOnInit(): void {
+    const target = document.querySelector('.tw');
+    const target2 = document.querySelector('.tw2');
+
+    const writer1 = new Typewriter(target, {
+      typeSpeed: 80,
+      deleteSpeed: 20,
+      cursorColor: '#efc2d5',
+      typeColor: '#272626',
+    });
+
+    const writer2 = new Typewriter(target2, {
+      typeSpeed: 80,
+      cursorColor: '#272626',
+      typeColor: '#efc2d5',
+      loop: true,
+    });
+    writer1
+      .type('Hello there. ')
+      .rest(300)
+      .type(' I am Hugo.')
+      .rest(500)
+      .removeCursor()
+      .start();
+
+    writer2
+      .rest(3000)
+      .type('fullstack development')
+      .rest(600)
+      .clear()
+      .type('main Angular & Spring')
+      .rest(600)
+      .clear()
+      .type('used to microservices')
+      .rest(600)
+      .clear()
+      .type('audio development')
+      .rest(600)
+      .clear()
+      .type('music production')
+      .rest(600)
+      .start();
+  }
 }
